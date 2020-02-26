5adce77add8e57968afd563f004343f0748afb52
wangyijie <wangyijieim@outlook.com>
2020-02-26 13:28:15 +0800

    image select

--- lib/util/image.dart
+++ lib/util/image.dart
@@ -1,22 +1,64 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:photo_view/photo_view.dart';
+import 'dart:async';
 
-class SingleImageView extends StatelessWidget {
+class SingleImageView extends StatefulWidget {
+
+  @override
+  _SingleImageViewState createState() => _SingleImageViewState(this.url);
 
   String url;
+
+  SingleImageView(this.url);
+}
+
+class _SingleImageViewState extends State<SingleImageView> {
+  String url;
+  bool change = false;
+
+  _SingleImageViewState(this.url);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text("查看头像"),
+          actions: <Widget>[
+            FlatButton(
+              textColor: Colors.white,
+              onPressed: () async  {
+                getImage();
+//                Navigator.of(context).pop();
+              },
+              child: Icon(
+                Icons.add_a_photo,
+                color: Colors.black,
+              ),
+              shape: CircleBorder(side: BorderSide(color: Colors.transparent)),
+            )
+          ],
+        ),
         body: Container(
             child: PhotoView(
-              imageProvider: NetworkImage(url),
+              imageProvider: change? FileImage(_image): NetworkImage(url),
             )
         ),
         bottomNavigationBar: null
     );
   }
 
-  SingleImageView(this.url);
+  File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = image;
+      change = true;
+    });
+  }
 }
