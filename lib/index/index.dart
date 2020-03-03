22e4f2632492e06f833bb3aae70ffdcfcbabeeab
wangyijie <wangyijieim@outlook.com>
2020-03-03 15:08:46 +0800

    new icon and noti icon

--- lib/index/index.dart
+++ lib/index/index.dart
@@ -116,7 +116,7 @@ class _IndexState extends State<Index> with TickerProviderStateMixin{
 
   void initNotifications() {
     // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
-    var initializationSettingsAndroid = AndroidInitializationSettings('app_icon');
+    var initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/noti');
     var initializationSettingsIOS = IOSInitializationSettings(
         onDidReceiveLocalNotification: onDidReceiveLocalNotification);
     var initializationSettings = InitializationSettings(
